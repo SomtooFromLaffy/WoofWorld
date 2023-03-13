8c43e9ef8c39d81c83e3d01611ddd9fbfe1e4b37
Somtoo Chukwu <somtoochukwu@Somtoos-MacBook-Pro-2.local>
2023-03-13 02:01:20 -0500

    Created About Us

---
+++ woofworld/src/components/AboutUs.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './AboutUsStyles.css'
+const AboutUs = () => {
+  return (
+    <div>
+        <section class="About" id="About">
+            <h2 class="heading"> About Us </h2>
+            <div class="mission-statements">
+                <div class="mission-text">
+                    <h2>Our History</h2>
+                    <p>We began in 5th century CE when we first domesticated dogs.
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Molestiae nisi nemo debitis assumenda saepe reiciendis nobis 
+                    cumque mollitia et veniam quis eos ullam aperiam, quia magnam 
+                    laboriosam ea quidem? Voluptates dolore quod ipsam ea quaerat 
+                    voluptate aliquam veritatis explicabo. Accusantium laborum sunt 
+                    repellendus fugit labore?
+                    </p>
+                </div>
+            
+                <div class="mission-text">
+                    <h2>Our Mission</h2>
+                    <p>We began in 5th century CE when we first domesticated dogs.
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Molestiae nisi nemo debitis assumenda saepe reiciendis nobis 
+                    cumque mollitia et veniam quis eos ullam aperiam, quia magnam 
+                    laboriosam ea quidem? Voluptates dolore quod ipsam ea quaerat 
+                    voluptate aliquam veritatis explicabo. Accusantium laborum sunt 
+                    repellendus fugit labore?
+                    </p>
+                </div>
+            
+                <div class="mission-text">
+                    <h2>Our Vision</h2>
+                    <p>We began in 5th century CE when we first domesticated dogs.
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Molestiae nisi nemo debitis assumenda saepe reiciendis nobis 
+                    cumque mollitia et veniam quis eos ullam aperiam, quia magnam 
+                    laboriosam ea quidem? Voluptates dolore quod ipsam ea quaerat 
+                    voluptate aliquam veritatis explicabo. Accusantium laborum sunt 
+                    repellendus fugit labore?
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default AboutUs
