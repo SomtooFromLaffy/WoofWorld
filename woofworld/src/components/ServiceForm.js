982415ef64877cee6cf4846014548c3773750474
Somtoo Chukwu <somtoochukwu@Somtoos-MacBook-Pro-2.local>
2023-03-15 06:19:03 -0500

    Form info can be collected and form returned

--- woofworld/src/components/ServiceForm.js
+++ woofworld/src/components/ServiceForm.js
@@ -3,64 +3,88 @@ import './ServiceFormStyles.css'
 
 
 function ServiceForm({ closeForm }) {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [petname, setPetname] = useState('');
-  const [instruction, setInstruction] = useState('');
+  const [formData, setFormData ] = useState({
+    name : '',
+    phone: '',
+    email: '',
+    petname: '',
+    service: [],
+    instruction: ''
+  });
+
+  const onChangeHandle = (e) =>{
+    if (e.target.name === 'service'){
+      let copy = { ...formData }
+      
+      if (e.target.checked){ 
+        copy.service.push(e.target.value) 
+      }else{
+        copy.service = copy.service.filter(s => s !== e.target.value)
+      }
+
+      setFormData(copy)
+    }else{
+      setFormData(() =>({...formData, 
+                            [e.target.name] : e.target.value}))
+    }
+  }
 
   const submitForm = (e) =>{
     e.preventDefault();
-    const formInfo = { name, phone, email, petname, instruction }
-    console.log(formInfo);
+    if (formData.service.length === 0){
+       alert('Please pick a service')
+    }else{
+      console.log(formData);
+      closeForm()
+    }
   }
   return (
     <div>
         <section className="form">
             <div>
-                <form>
-                {/* <form onSubmit={submitForm}> */}
+                
+                <form onSubmit={submitForm}>
                     <h2>Service Request Form</h2>
                     <label> Name </label>
                     <input type="text"
-                           value = {name} 
-                           onChange = {(e) => setName(e.target.value)}
+                           name = 'name' 
+                           onChange = {onChangeHandle}
                            required />
                     
                     <label>Phone </label>
                     <input type="tel"
-                           value = {phone} 
-                           onChange = {(e) => setPhone(e.target.value)}
+                           name = 'phone' 
+                           onChange = {onChangeHandle}
                            required />
                     
                    <label>Email </label>
                    <input type="email"
-                           value = {email} 
-                           onChange = {(e) => setEmail(e.target.value)}
+                           name = 'email' 
+                           onChange = {onChangeHandle}
                            required />
 
                     <label> Pet Name </label>
                     <input type="text"
-                           value = {petname} 
-                           onChange = {(e) => setPetname(e.target.value)}
+                           name = 'petname' 
+                           onChange = {onChangeHandle}
                            required />
 
                    <fieldset>
                         <legend>Select Service</legend>
-                        <label> <input type="checkbox" /> Grooming </label>
-                        <label> <input type="checkbox" /> Vet</label>
-                        <label> <input type="checkbox" /> Boarding </label>
-                        <label> <input type="checkbox" /> Dog Walking </label>
-                        <label> <input type="checkbox" /> Vaccinations </label>
+                        <label> <input type="checkbox"  name='service' value='Grooming' onChange = {onChangeHandle} /> Grooming </label>
+                        <label> <input type="checkbox" name='service' value='Vet' onChange = {onChangeHandle} /> Vet</label>
+                        <label> <input type="checkbox" name='service' value='Boarding' onChange = {onChangeHandle} /> Boarding </label>
+                        <label> <input type="checkbox" name='service' value='Dog Walk' onChange = {onChangeHandle} /> Dog Walking </label>
+                        <label> <input type="checkbox" name='service' value='Vaccine' onChange = {onChangeHandle} /> Vaccinations </label>
                         <p><em>At this time, our accesory service will only be in store until our developer gets to building the online store </em></p>
                     </fieldset>
                    <p><em>Fill in appointment day and time below</em></p>
 
                     <label>Special Instructions</label>
                     <textarea                            
-                            value = {instruction} 
-                           onChange = {(e) => setInstruction(e.target.value)}
-                           required></textarea>
+                            name = 'instruction' 
+                           onChange = {onChangeHandle}
+                           ></textarea>
 
                     <div className='options'>
                         <button type = 'button' onClick={closeForm}>Cancel</button>
